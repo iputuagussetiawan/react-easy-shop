d6ac05524f024b9d7eda8dc903b43cb8432b608a
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-02-20 16:35:23 +0800

    299. Consume Visitor Details API Form Client

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,9 +9,16 @@ import FeaturedProducts from '../components/home/FeaturedProducts'
 import HomeTop from '../components/home/HomeTop'
 import HomeTopMobile from '../components/home/HomeTopMobile'
 import NewArrival from '../components/home/NewArrival'
+import AppURL from '../api/AppURL'
+import axios from 'axios'
 export class HomePage extends Component {
     componentDidMount(){
-        window.scroll(0,0)
+        window.scroll(0,0);
+        this.GetVisitorDetails();
+    }
+
+    GetVisitorDetails =()=>{
+        axios.get(AppURL.VisitorDetails).then().catch()
     }
     render() {
         return (
