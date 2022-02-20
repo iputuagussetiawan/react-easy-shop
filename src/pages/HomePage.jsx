9ec684fa9818aca784b3034a694952b1e887da80
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-02-20 09:35:14 +0800

    284. Footer View Mobile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import FooterDesktop from '../components/common/FooterDesktop'
+import FooterMobile from '../components/common/FooterMobile'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import NavMenuMobile from '../components/common/NavMenuMobile'
 import Categories from '../components/home/Categories'
@@ -24,7 +25,13 @@ export class HomePage extends Component {
                 <NewArrival></NewArrival>
                 <Categories></Categories>
                 <Collection></Collection>
-                <FooterDesktop></FooterDesktop>
+                <div className="Desktop">
+                    <FooterDesktop/>
+                </div>
+                <div className="Mobile">
+                    <FooterMobile/>
+                </div>
+                
             </Fragment>
         )
     }
