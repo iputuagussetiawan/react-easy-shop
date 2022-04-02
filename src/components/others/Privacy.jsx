905f9e77220fa5435f8b2a5686020f7d2e4d7c3b
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 11:07:28 +0800

    311. Consume Site Info API in Client Side Part 5

--- src/components/others/Privacy.jsx
+++ src/components/others/Privacy.jsx
@@ -3,12 +3,16 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
 import parse from 'html-react-parser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class Privacy extends Component {
     constructor(){
         super();
         this.state={
-            privacy:""
+            privacy:"",
+            loaderDiv:"",
+            mainDiv:"d-none"
         }
     }
 
@@ -17,7 +21,7 @@ class Privacy extends Component {
              let StatusCode = response.status;
              if(StatusCode==200){
                   let JsonData = (response.data)[0]['privacy'];
-                  this.setState({privacy:JsonData});
+                  this.setState({privacy:JsonData,loaderDiv:"d-none",mainDiv:""});
              } 
 
         }).catch(error=>{
@@ -30,10 +34,42 @@ class Privacy extends Component {
                 <Container>
                     <Row className="p-2">
                         <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-                            <h4 className="section-title-login">Privacy Page </h4>
-                            <p className="section-title-contact">
-                            {parse(this.state.privacy)}
-                            </p>
+                            <div className={this.state.loaderDiv}>
+                                <div class="ph-item">
+                                    <div class="ph-col-12">        
+                                        <div class="ph-row">           
+                                            <div class="ph-col-4"></div>
+                                            <div class="ph-col-8 empty"></div>
+                                            <div class="ph-col-6"></div>
+                                            <div class="ph-col-6 empty"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ph-item">
+                                    <div class="ph-col-12">        
+                                        <div class="ph-row">           
+                                            <div class="ph-col-4"></div>
+                                            <div class="ph-col-8 empty"></div>
+                                            <div class="ph-col-6"></div>
+                                            <div class="ph-col-6 empty"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                            <div class="ph-col-12"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={this.state.mainDiv}>
+                                <h4 className="section-title-login">Privacy Page </h4>
+                                <p className="section-title-contact">
+                                {parse(this.state.privacy)}
+                                </p>
+                            </div>
                        </Col>
                     </Row>
                </Container>
