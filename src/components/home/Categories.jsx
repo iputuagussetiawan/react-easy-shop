c0d922032dd3d67d1c0bdc5808c36f5efc27c1db
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 18:59:08 +0800

    322. Consume Product List API Part 2

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -20,7 +20,7 @@ class Categories extends Component {
     render() {
         const CatList = this.state.MenuData;
         const MyView = CatList.map((CatList,i)=>{
-             return <Col key={i.toString()} className="p-0" key={1} xl={2} lg={2} md={2} sm={6} xs={6}>
+             return <Col key={i.toString()} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
                 <Card className="h-100 w-100 text-center">          
                     <Card.Body> 
                         <img className="center" src={CatList.category_image} />  
