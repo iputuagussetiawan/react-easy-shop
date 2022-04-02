877a88734fdf1732ddffc60cb37da61180c91fb8
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 12:58:20 +0800

    315. Consume Category Subcategory API Part 1

--- src/components/home/HomeTop.jsx
+++ src/components/home/HomeTop.jsx
@@ -3,15 +3,31 @@ import {Container,Row,Col} from 'react-bootstrap'
 import Slider from './Slider'
 import MegaMenu from './MegaMenu'
 import HomeSlider from './HomeSlider'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
 class HomeTop extends Component {
+    constructor(){
+        super();
+        this.state ={
+            MenuData:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get(AppURL.AllCategoryDetails).then(response =>{
+            this.setState({MenuData:response.data});
+        }).catch(error=>{
+
+        });
+    }
     render() {
         return (
             <Fragment>
                 <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                     <Row>
                         <Col lg={3} md={3} sm={12}>
-                            <MegaMenu />
+                            <MegaMenu data={this.state.MenuData} />
                         </Col>
 
                         <Col lg={9} md={9} sm={12}>
