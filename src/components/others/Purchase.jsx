ba4564bc70caf60403ae54b459f6d90bf773aa8a
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 10:05:55 +0800

    309. Consume Site Info API in Client Side Part 3

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -3,36 +3,53 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
 import parse from 'html-react-parser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class Purchase extends Component {
     constructor(){
         super();
         this.state={
-            parchase_guide:""
+            purchase:""
         }
     }
 
     componentDidMount(){
-        axios.get(AppURL.AllSiteInfo).then(response =>{
-             let StatusCode = response.status;
-             if(StatusCode==200){
-                  let JsonData = (response.data)[0]['parchase_guide'];
-                  this.setState({parchase_guide:JsonData});
-             } 
+        let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
+        if(SiteInfoPurchase == null){
+            axios.get(AppURL.AllSiteInfo).then(response =>{
+                let StatusCode = response.status;
+                if(StatusCode==200){
+                    let JsonData = (response.data)[0]['parchase_guide'];
+                    this.setState({purchase:JsonData});
+                    sessionStorage.setItem("SiteInfoPurchase",JsonData)
+                } 
+                else{
+                    toast.error("Somthing Went Wrong",{
+                        position: "bottom-center"
+                    });
+                }
+            }).catch(error=>{
+                toast.error("Somthing Went Wrong",{
+                        position: "bottom-center"
+                });
+            });
 
-        }).catch(error=>{
-
-        });
-    }
+        }  // end If Conditon 
+        else{
+            this.setState({purchase:SiteInfoPurchase});
+        }
+    } 
     render() {
         return (
             <Fragment>
                 <Container>
+                <ToastContainer />
                     <Row className="p-2">
                         <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
                             <h4 className="section-title-login">Purchase Page </h4>
                             <p className="section-title-contact">
-                            {parse(this.state.parchase_guide)}
+                            {parse(this.state.purchase)}
                             </p>
                         </Col>
                     </Row>
